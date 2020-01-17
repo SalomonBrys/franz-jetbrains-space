49f8ed879893db4b5d530d08ff60a84d2d94bf0d
Salomon BRYS <salomon@kodein.net>
2020-01-17 12:17:21 +0100

    Fixed a bug when there is no count

--- webview.js
+++ webview.js
@@ -4,8 +4,8 @@ const SELECTOR_COUNTERS = '.AppStyles-Navigation-hasUpdatesCounterContainer';
 
 module.exports = Franz => {
   const getMessages = () => {
-    const divs = document.querySelectorAll(SELECTOR_COUNTERS);
-    const count = Array.from(divs).map(d => parseInt(d.innerText)).reduce((l, r) => l + r);
+    const divs = Array.from(document.querySelectorAll(SELECTOR_COUNTERS));
+    const count = (divs.length > 0) ? divs.map(d => parseInt(d.innerText)).reduce((l, r) => l + r) : 0;
     Franz.setBadge(count);
   };
 
